096467e1ae4dbf4f4de3e3fb66622b9788f34b68
Marco Antonio <marco.perez.cetis76@gmail.com>
2023-02-22 12:08:53 -0600

    prueba 1

---
+++ c61f6390d3b248395213.js
@@ -0,0 +1,10 @@
+import './main.css'
+
+import {
+    router
+} from './router/index.routes';
+
+router(window.location.hash)
+window.addEventListener('hashchange', () => {
+    router(window.location.hash)
+})
